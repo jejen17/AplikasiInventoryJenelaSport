299d35455b4ad2bd31ff4c2ab4983dcdbca20cac
jeshin <pbaan7728@gmail.com>
2026-01-03 23:18:52 +0700

    update

--- app/src/main/java/com/example/kkp/HistoryFragment.java
+++ app/src/main/java/com/example/kkp/HistoryFragment.java
@@ -1,64 +1,62 @@
 package com.example.kkp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+
 public class HistoryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView tabSemua, tabMasuk, tabKeluar;
 
     public HistoryFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryFragment newInstance(String param1, String param2) {
-        HistoryFragment fragment = new HistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tabSemua = view.findViewById(R.id.tabSemua);
+        tabMasuk = view.findViewById(R.id.tabMasuk);
+        tabKeluar = view.findViewById(R.id.tabKeluar);
+
+        tabSemua.setOnClickListener(v -> updateFilterUI(tabSemua));
+        tabMasuk.setOnClickListener(v -> updateFilterUI(tabMasuk));
+        tabKeluar.setOnClickListener(v -> updateFilterUI(tabKeluar));
+
+    }
+
+    private void updateFilterUI(TextView selectedTab) {
+        Typeface fontBold = ResourcesCompat.getFont(requireContext(), R.font.opensansbold);
+        Typeface fontRegular = ResourcesCompat.getFont(requireContext(), R.font.opensansreguler);
+
+        TextView[] allTabs = {tabSemua, tabMasuk, tabKeluar};
+
+        for (TextView tab : allTabs) {
+            if (tab == selectedTab) {
+                tab.setBackgroundResource(R.drawable.bg_filter_active);
+                tab.setTextColor(ContextCompat.getColor(requireContext(), R.color.white));
+                tab.setTypeface(fontBold);
+            } else {
+                tab.setBackgroundResource(R.drawable.bg_filter_inactive);
+                tab.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_input));
+                tab.setTypeface(fontRegular);
+            }
+        }
     }
 }
